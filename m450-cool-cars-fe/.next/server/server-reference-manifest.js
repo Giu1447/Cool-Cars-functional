fa4a7b4b5737ad76153a5c03c3d1f7f3c9d2ddb6
klionCH <sven.luebcke@lernende.bbw.ch>
2024-11-27 14:01:31 +0100

    add gitignore

---
+++ m450-cool-cars-fe/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xxnr0kzmeJOmR1T2+nNwjzbxp+ucu5ghI9RK7eWshp0=\"\n}"
--- m450-cool-cars-fe/src/app/carform/page.js
+++ m450-cool-cars-fe/src/app/carform/page.js
@@ -1,10 +1,7 @@
-
-
 export default function CarForm() {
   return (
     <div>
       <h1>Car Form</h1>
-
     </div>
   )
 }
