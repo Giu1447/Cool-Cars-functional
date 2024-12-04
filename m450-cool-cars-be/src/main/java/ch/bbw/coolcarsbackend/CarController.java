e7aeb31bbd4754be341bc0875d9ada3666e20b88
Giu1447 <giulian.mazzeo@lernende.bbw.ch>
2024-12-04 14:57:13 +0100

    localhost:3000

--- m450-cool-cars-be/src/main/java/ch/bbw/coolcarsbackend/CarController.java
+++ m450-cool-cars-be/src/main/java/ch/bbw/coolcarsbackend/CarController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3001")
+@CrossOrigin("http://localhost:3000")
 public class CarController implements ApplicationRunner {
 
     @Autowired
