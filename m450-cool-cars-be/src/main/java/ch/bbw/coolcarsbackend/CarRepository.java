2cd719f154f18a0bedc116bb10386f01f7a593c8
klionCH <sven.luebcke@lernende.bbw.ch>
2024-12-04 13:53:59 +0100

    fügt paging hinzu als api call

--- m450-cool-cars-be/src/main/java/ch/bbw/coolcarsbackend/CarRepository.java
+++ m450-cool-cars-be/src/main/java/ch/bbw/coolcarsbackend/CarRepository.java
@@ -3,6 +3,10 @@ package ch.bbw.coolcarsbackend;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 @Repository
-public interface CarRepository extends CrudRepository<Car, Integer> {
+public interface CarRepository extends JpaRepository<Car, Integer> {
+    // Keine zusätzlichen Methoden erforderlich
 }
+
