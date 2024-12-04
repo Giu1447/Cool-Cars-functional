5dac5f5071231348a3e4da09b965ebf8fc163bc4
Giu1447 <giulian.mazzeo@lernende.bbw.ch>
2024-12-04 14:32:11 +0100

    remove .next

--- m450-cool-cars-fe/.next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LxmxPjhS6/kR4Y44TwEhrBo5sQ1djGPEOt7O9Sgh9oY=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zfM2fwZ6/Clh9vtSWqiStMhuL0PL2ppheGYlOzOLOig=\"\n}"
->>>>>>> 61a5d63f6cce57235341386070e3d1d7620946f9
