39f342acc3068f2886492e373bdd2a333571116d
Giu1447 <giulian.mazzeo@lernende.bbw.ch>
2025-01-08 13:12:28 +0100

    carform

--- m450-cool-cars-fe/src/app/carform/page.js
+++ m450-cool-cars-fe/src/app/carform/page.js
@@ -1,7 +1,55 @@
+"use client";
+import { useState } from "react";
+
 export default function CarForm() {
-  return (
-    <div>
-      <h1>Car Form</h1>
-    </div>
-  )
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+
+        fetch("http://localhost:8080/cars", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setIsSubmitted(true);
+            })
+            .catch((error) => console.error("Error:", error));
+    }
+
+    if (isSubmitted) {
+        window.location.href = "/";
+    }
+
+
+    return (
+        <div>
+            <h1>Car Form</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Brand:
+                    <input type="text" name="brand" />
+                </label>
+                <br />
+                <label>
+                    Model:
+                    <input type="text" name="model" />
+                </label>
+                <br />
+                <label>
+                    Horsepower:
+                    <input type="number" name="horsePower" min={0}/>
+                </label>
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
